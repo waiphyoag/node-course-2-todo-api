aab1015d447e093da53475f5b3e103e253d528c8
waiphyoaung <waiphyoag1993@gmail.com>
2017-04-20 19:26:47 +0630

    Init commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,41 @@
+// const MongoClient = require('mongodb').MongoClient;
+
+const {MongoClient, ObjectID} = require('mongodb');   // Object destructuring
+
+// Object Destructuring Sample
+// var user = {name: 'Wai Phyo Aung', age: 23};
+// var {name} = user;
+// console.log(name);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDb server');
+  }
+
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to do',
+  //   completed: false
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert todo', err);
+  //   }
+  //
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
+
+  // db.collection('Users').insertOne({
+  //   name: 'Wai Phyo Aung',
+  //   age: 23,
+  //   location: 'Myanmar'
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert user', err);
+  //   }
+  //
+  //   console.log(result.ops[0]._id.getTimestamp());
+  // });
+
+  db.close();
+});
